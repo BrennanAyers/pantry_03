62bf4792fbdfbc15c3f97d9ab03644f5a6f2c487
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-10 11:12:37 -0600

    Add initial summary method to CookBook class, fix mac_and_cheese ingredient additions in Cookbook test

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -8,4 +8,21 @@ class CookBook
   def add_recipe(recipe)
     recipes << recipe
   end
+
+  def summary
+    recipes.map do |recipe|
+      {
+        name: recipe.name,
+        details: {
+          ingredients: recipe.ingredients.map do |ingredient|
+            {
+              ingredient: ingredient.name,
+              amount: recipe.quantity_of(ingredient).to_s + " " + ingredient.unit
+            }
+          end,
+          total_calories: recipe.total_calories
+        }
+      }
+    end
+  end
 end
